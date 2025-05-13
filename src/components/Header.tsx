99c4596111e0301d69edcf7b499b0c2fc323a37a
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-14 00:36:47 +0200

    div angepasst

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,12 @@ import "../styles/Header.css";
 const Header = () => (
   <header id='home' className="header">
     <div className="overlay">
-      <h1>Willkommen bei J. Landolt</h1>
-      <h2>Gartenbau & Gartenpflege</h2>
+      <h1>J. Landolt Gartenbau</h1>
+      <div className="overlay-text">
+        <h2>Gartenbau & Gartenpflege</h2>
+        <p>Bülach, Kloten & Rümlang</p>
+      </div>
+      
     </div>
   </header>
 );
