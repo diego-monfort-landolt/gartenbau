8249d1d548f30449be1f356e129677db3889e35f
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-14 14:57:36 +0200

    änderung der galery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,5 @@
+import { useState } from "react";
 import "../styles/Gallery.css";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 import Foto1 from '../assets/img/gartenplatten-wiese.png';
 import Foto2 from '../assets/img/J. Landolt Gartenbau GmbH,gartenweg.png';
@@ -9,54 +7,26 @@ import Foto3 from '../assets/img/schönerplattenboden.png';
 import Foto4 from '../assets/img/kleinemauer.png';
 import Foto5 from '../assets/img/J.LandoltGartenbauGmbH.png';
 import Foto6 from '../assets/img/garten-j-landolt.png';
-import { useState } from "react";
 
-const images = [
-  Foto1,
-  Foto2,
-  Foto3,
-  Foto4,
-  Foto5,
-  Foto6,
-  "https://cdn.pixabay.com/photo/2014/07/05/08/30/lawn-mower-384589_640.jpg",
-  "https://cdn.pixabay.com/photo/2014/07/25/19/45/landscape-gardener-401880_640.jpg"
-];
+const images = [Foto1, Foto2, Foto3, Foto4, Foto5, Foto6];
 
 const Gallery = () => {
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    center: true,
-    autoplaySpeed: 3000
-  };
+  const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <section className="gallery">
       <h2 className="gallery-title">Galerie</h2>
-      <div className="gallery-box">
-        <Slider {...settings}>
-          {images.map((src, index) => (
-            <div key={index} className="gallery-item-container">
-              <img
-                src={src}
-                alt="Gartenbild"
-                className="gallery-item"
-                onClick={() => setSelectedImage(src)}
-              />
-            </div>
-          ))}
-        </Slider>
+      <div className="gallery-grid">
+        {images.map((src, index) => (
+          <div key={index} className="gallery-item">
+            <img src={src} alt={`Bild ${index + 1}`} onClick={() => setSelectedImage(src)} />
+          </div>
+        ))}
       </div>
 
       {selectedImage && (
-         <div className="lightbox">
-          <button className="lightbox-close" onClick={() => setSelectedImage(null)}>✖</button>
+        <div className="lightbox" onClick={() => setSelectedImage(null)}>
+          <button className="lightbox-close">✖</button>
           <img src={selectedImage} alt="Vergrößertes Bild" />
         </div>
       )}
