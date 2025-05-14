c3569c0e238ad03d11f4d3c14d044cc615f4f645
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-14 09:05:22 +0200

    hinzufügen mehr abstände

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import "../styles/Header.css";
 const Header = () => (
   <header id='home' className="header">
     <div className="overlay">
-      <h1>J. Landolt Gartenbau</h1>
+      <h1>J. Landolt Gartenbau GmbH</h1>
       <div className="overlay-text">
         <h2>Gartenbau & Gartenpflege</h2>
         <p>Bülach, Kloten & Rümlang</p>
