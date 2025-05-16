80432c990373967cac512cf8e7654496f67fc3bd
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-16 10:16:52 +0200

    hinzufügen whatsapp und funktion im header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import "../styles/Header.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa"
 
 
 const Header = () => (
@@ -8,6 +9,13 @@ const Header = () => (
       <h1>J. Landolt Gartenbau GmbH</h1>
       <div className="overlay-text">
         <h2>Gartenbau & Gartenpflege</h2>
+         <a
+            href="https://wa.me/41791234567"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="whatsapp-button"
+          ><FaWhatsapp className="whatsapp-icon" size={28} color="#25D366"  />
+          </a>
         <p><FaMapMarkerAlt style={{ marginRight: '5px', transform: 'rotate(-15deg)' }} />Bülach | Kloten | Rümlang</p>
       </div>
     </div>
