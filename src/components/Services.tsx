204be5c7c01ae9ca36bca29f1725061029c4eafe
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-20 13:55:12 +0200

    text verbessert

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -71,8 +71,8 @@ const Services = () => {
       <section id="Dienstleistungen" className="services">
         <h2 className="services-title">Unsere Dienstleistungen</h2>
           <p className='descriton-gall'>
-            Ihr Fachbetrieb für <b>Gartenbau in Bülach, Kloten und Rümlang</b> 
-             Mit langjähriger Erfahrung und echter Leidenschaft gestalten und pflegen wir individuelle Gärten – ganz nach Ihren Vorstellungen. Ob Neuanlage, Umgestaltung oder saisonale Pflege: Bei <b>J. Landolt Gartenbau GmbH</b> stehen <em>Engagement, Zuverlässigkeit und persönliche Beratung</em> im Mittelpunkt. Jorge Landolt und sein Team sind Ihr Ansprechpartner für professionellen Gartenbau im Zürcher Unterland.
+            Ihr Fachbetrieb für <b>Gartenbau in Bülach, Kloten und Rümlang</b><br />
+            Mit langjähriger Erfahrung und echter Leidenschaft gestalten und pflegen wir individuelle Gärten – ganz nach Ihren Vorstellungen. Ob Neuanlage, Umgestaltung oder saisonale Pflege: Bei <b>J. Landolt Gartenbau GmbH</b> stehen <em>Engagement, Zuverlässigkeit und persönliche Beratung</em> im Mittelpunkt. Jorge Landolt und sein Team sind Ihr Ansprechpartner für professionellen Gartenbau im Zürcher Unterland.
 
             Zu unseren Arbeiten zählen Natursteinarbeiten, Rasenpflege, Hecken- und Baumschnitt sowie die kreative Gestaltung von Gartenwegen und Sitzplätzen.
       </p>
