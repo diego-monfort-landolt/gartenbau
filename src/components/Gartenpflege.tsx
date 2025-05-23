2d52fb2a1b817c8ccba63e795d8703ea99a74f35
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-23 08:40:43 +0200

    prüfe code und entferne unnötige lehrzeichen

--- src/components/Gartenpflege.tsx
+++ src/components/Gartenpflege.tsx
@@ -14,12 +14,10 @@ const Gartenpflege: React.FC = () => {
           Ein gepflegter Garten ist mehr als nur eine Außenfläche – er ist ein Rückzugsort, eine Wohlfühloase und ein lebendiges Naturerlebnis. Mit unserer fachkundigen Gartenpflege sorgen wir dafür, dass Ihre Grünflächen gesund und blühend bleiben – das ganze Jahr über. Ob regelmäßige Pflege, Rasen- und Heckenservice oder gezielte Maßnahmen zum Werterhalt – wir kümmern uns mit Erfahrung und Leidenschaft um jeden Bereich Ihres Gartens.
 
           Unsere Gartenbau-Dienstleistungen verwandeln Ihre Ideen in Wirklichkeit. Ob Neu- oder Umgestaltung, moderne oder natürliche Designs – wir planen und realisieren individuelle Gartenkonzepte nach Ihren Wünschen. Durch kreative Gestaltung, hochwertige Materialien und präzise Umsetzung schaffen wir stilvolle Außenbereiche, die Ästhetik und Funktionalität perfekt vereinen.
-
           Lassen Sie Ihren Garten in neuem Glanz erstrahlen. <br/><b>Wir freuen uns auf Ihre Anfrage!</b>
         </p>
       </div>
     </section>
   );
 };
-
 export default Gartenpflege;
