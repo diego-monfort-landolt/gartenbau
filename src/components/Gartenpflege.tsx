7085de7cacb0cfde058813463b784971303b062b
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-27 08:09:24 +0200

    hinzufügen neues textes -hinzufügen von helmet

--- src/components/Gartenpflege.tsx
+++ src/components/Gartenpflege.tsx
@@ -1,25 +1,37 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import "../styles/Gartenpflege.css";
 import BeispielBild from "../assets/img/J. Landolt Gartenbau GmbH,gartenweg.png";
 
 const Gartenpflege: React.FC = () => {
   return (
-    <section id="Gartenpflege" className="gartenpflege-container">
+  <>
+    <Helmet>
+        <title>Gartenpflege | J. Landolt Gartenbau GmbH</title>
+        <meta 
+          name="description" 
+          content="Professionelle Gartenpflege in Bülach und Umgebung. Wir gestalten und pflegen private Gärten, Wohnsiedlungen und Parkanlagen mit Leidenschaft und Expertise. Lassen Sie uns Ihre Balkon-Bepflanzung oder Dachbegrünung übernehmen. Kontaktieren Sie uns für eine individuelle Beratung! Landolt Gartenbau, Buelach, Kloten, Rümlang"
+        />
+        <meta name="keywords" content="Gartenpflege, Gartenbau, Bülach, Garten gestalten, Parkanlagen, Dachbegrünung, Balkonbepflanzung, Pflanzenpflege" />
+        <meta name="author" content="J. Landolt Gartenbau GmbH" />
+        <meta name="robots" content="index, follow" />
+      </Helmet>
+   <section id="Gartenpflege" className="gartenpflege-container">
       <div className="image-wrapper">
         <img src={BeispielBild} alt="Gartenpflege und Gartenbau Beispiel" />
       </div>
       <div className="text-wrapper">
         <h2>Gartenpflege</h2>
         <p>
-          Ein gepflegter Garten ist mehr als nur eine Außenfläche – er ist ein Rückzugsort, eine Wohlfühloase und ein lebendiges Naturerlebnis. Mit unserer fachkundigen Gartenpflege sorgen wir dafür, dass Ihre Grünflächen gesund und blühend bleiben – das ganze Jahr über. Ob regelmäßige Pflege, Rasen- und Heckenservice oder gezielte Maßnahmen zum Werterhalt – wir kümmern uns mit Erfahrung und Leidenschaft um jeden Bereich Ihres Gartens.
-          Unsere Gartenbau-Dienstleistungen verwandeln Ihre Ideen in Wirklichkeit. Ob Neu- oder Umgestaltung, moderne oder natürliche Designs – wir planen und realisieren individuelle Gartenkonzepte nach Ihren Wünschen. Durch kreative Gestaltung, hochwertige Materialien und präzise Umsetzung schaffen wir stilvolle Außenbereiche, die Ästhetik und Funktionalität perfekt vereinen.
-          Lassen Sie Ihren Garten in neuem Glanz erstrahlen. <br/>
-          <h3>Gartenpflege nach Maß – abgestimmt auf Ihre Bedürfnisse</h3>
-          <p>Unser Service umfasst die regelmäßige Gartenpflege, Rasenmähen, Heckenschnitt, Beetpflege und saisonale Arbeiten wie Laubentfernung oder Pflanzenschnitt. Auch gezielte Maßnahmen zur Werterhaltung Ihrer Gartenanlage gehören zu unserem Leistungsangebot. Mit langjähriger Erfahrung, einem geschulten Auge und viel Leidenschaft kümmern wir uns um jeden Bereich Ihres Gartens.</p>
-          <b>Wir freuen uns auf Ihre Anfrage!</b>
+         Unsere Stärken sind die Pflege von privaten Gärten, Wohnsiedlungen und Parkanlagen. Auch für Ihre Balkon-Bepflanzung oder Dachbegrünung sind wir die richtige Wahl.
+          Wir übernehmen für Sie die Pflege Ihrer Pflanzen, als einmaligen Auftrag oder in wiederkehrenden Intervallen. Wir gehen Ihnen gern auch sporadisch zur Hand, wenn Ihnen die Zeit fehlt oder Sie andere Prioritäten setzen wollen.  
+          <br />
+          Um Ihren Unterhaltsbedarf zu klären und Ihre Wünsche zu erfüllen können Sie uns gerne kontaktieren.
         </p>
       </div>
     </section>
+  </>
+   
   );
 };
 export default Gartenpflege;
