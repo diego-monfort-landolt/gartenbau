eef418a247f89237a911e614f3a914df53e46923
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-05-28 00:41:32 +0200

    aenderung in der base und json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://gartenbaulandolt.ch'
+  base:'https://www.gartenbaulandolt.ch'
 })
