856be703c0d65509e61db83714522dac538ef69f
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-06-09 19:02:46 +0200

    entferne lehrzeichen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,6 @@ const Header = () => (
       name="description" 
       content="Gartenbau und Gartenpflege in Bülach und Umgebung - J. Landolt Gartenbau GmbH – Kreative Gartengestaltung, Natursteinarbeiten und nachhaltige Pflege für stilvolle Außenbereiche." 
       />
-
       <meta name="keywords" content="Gartenbau, Gartenpflege, Bülach, Kloten, Rümlang, Landschaftsgestaltung, Natursteinarbeiten, Dachbegrünung, Gartenplanung, Gärtner" />
       <meta name="author" content="J. Landolt Gartenbau GmbH" />
       <meta name="robots" content="index, follow" />
@@ -23,7 +22,6 @@ const Header = () => (
       <div className="overlay-text">
         <h1>Gartenbau & Gartenpflege<br/> 
         in Bülach und Umgebung</h1>
-
        <a href="tel:+41764197508" className="phone-button">
          <FaPhone 
             className="phone-icon" 
@@ -44,6 +42,5 @@ const Header = () => (
     </div>
   </header>
   </>
- 
 );
 export default Header;
